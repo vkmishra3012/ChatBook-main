270ef7deef81475493f0e14da0fa0bbd50621dc7
Vivek <vkmishra3012@gmail.com>
2024-03-07 11:52:15 +0530

    config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,16 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore, collection } from "firebase/firestore";
-require('dotenv').config()    
+import env from "react-dotenv"; 
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId
+  apiKey: env.apiKey,
+  authDomain: env.authDomain,
+  projectId: env.projectId,
+  storageBucket: env.storageBucket,
+  messagingSenderId: env.messagingSenderId,
+  appId: env.appId,
+  measurementId: env.measurementId
 };
 
 const app = initializeApp(firebaseConfig);
